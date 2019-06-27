dc5d2e9717c76d13d75941aae23839d5b891d320
Luiz Nascimento <luizh.nnh@gmail.com>
2019-06-27 10:50:29 -0300

    Add dashboard and config includes

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'pharma_market');
+define('DB_USER', 'root');
+define('DB_PASS', 'df2019@%');
+
--- index.php
+++ index.php
@@ -1,41 +1,9 @@
-<!doctype html>
-<html lang="pt-BR">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php 
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/custom.css">
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/d41c6c27a7.js"></script>
+include_once('includes/_header.php');
 
-    <title>SGM - SysMarket v1.0</title>
-  </head>
-  <body>
-    <div class="content">
-        <div class="loginbox">
-            <h4>SGM - SysMarket</h4>
-            <form>
-                <div class="form-group">
-                    <label for="formGroupExampleInput">Login</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Username/Email">
-                </div>
-                <div class="form-group">
-                    <label for="formGroupExampleInput2">Senha</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Password">
-                </div>
-                <div>
-                    <button type="button" class="btn btn-secondary float-right">Entrar <i class="fas fa-sign-in-alt"></i></button>
-                </div>
-            </form>            
-        </div>
-    </div>
+?>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
+
+
+<?php include_once('includes/_footer.php'); ?>
